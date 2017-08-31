727b0ebcd3b2ea90b1c693513dad1c04a91b6214
Drew Morris <dhmorris@gmail.com>
2017-08-30 21:58:29 -0400

    Integrating with avataria

--- hazel-wiki/app/controllers/compendiaController.js
+++ hazel-wiki/app/controllers/compendiaController.js
@@ -1,8 +1,9 @@
 "use strict";
 
-const CompendiaViewModel = require("../models/homeViewModel");
+const CompendiaViewModel = require('../models/homeViewModel');
+const avataria = require('avataria');
 
-const _ = require("lodash");
+const _ = require('lodash');
 
 class CompendiaController {
   constructor(server, config, authMethod, documentRepository, searchProvider, analyticsService) {
@@ -19,6 +20,8 @@ class CompendiaController {
   _bindRoutes() {
     // /compendia
     this._server.get("/compendia", this._auth, this.index.bind(this));
+    // /compendia/avataria
+    this._server.post("/compendia/avataria", this._auth, this.avataria.bind(this));
   }
 
   /**
@@ -30,6 +33,26 @@ class CompendiaController {
     viewModel.config = this._config;
 
     res.render("compendia-home", viewModel);
+  }
+
+  /**
+   * Respond with the avatar
+   */
+  avataria(req, res, next) {
+    const { body } = req;
+    const { gender, species } = body;
+    const options = {
+      gender,
+      species,
+    };
+
+    // generate from avataria
+    avataria(options, function (err, data) {
+      const { base64 } = data;
+      res.send({
+        base64,
+      })
+    });
   }
 }
 
--- hazel-wiki/app/themes/default/public/scripts/compendia.js
+++ hazel-wiki/app/themes/default/public/scripts/compendia.js
@@ -1,34 +1,66 @@
 var SELECTORS = {
-    FORM: "form",
-    DROPZONE: ".js-dropzone"
+  AVATARIA: '#avataria',
+  AVATARIA_OUTPUT: '#avataria-output',
+  AVATARIA_GENDER: '#avataria-gender',
+  AVATARIA_SPECIES: '#avataria-species',
+  DROPZONE: '.js-dropzone'
+};
+
+var ROUTES = {
+  AVATARIA: '/compendia/avataria'
 };
 
 /**
  * Page View Class for the Document Edit page.
  */
 var CompendiaPage = function($scope) {
-    this.$scope = $scope;
+  this.$scope = $scope;
 
-    this.dropzone = new Dropzone(SELECTORS.DROPZONE);
+  this.dropzone = new Dropzone(SELECTORS.DROPZONE);
+  this.$avataria = $scope.find(SELECTORS.AVATARIA);
+  this.$avatariaOutput = $scope.find(SELECTORS.AVATARIA_OUTPUT);
+  this.$avatariaGender = $scope.find(SELECTORS.AVATARIA_GENDER);
+  this.$avatariaSpecies = $scope.find(SELECTORS.AVATARIA_SPECIES);
 
-    this.bind();
+  this.bind();
 };
 CompendiaPage.constructor = CompendiaPage;
 CompendiaPage.prototype = {
-    bind: function () {
-        this.dropzone.on("success", this.onDropzoneSuccess.bind(this));
-    },
+  bind: function () {
+    this.dropzone.on('success', this.onDropzoneSuccess.bind(this));
+    this.$avataria.on('click', this.onAvatariaClick.bind(this));
+  },
 
-    /**
-     * Handle the event when the user successfully uploads a file via Dropzone
-     * @prop file [string]
-     * @prop responseText [string]
-     */
-    onDropzoneSuccess: function (file, responseText) {
-        console.log(file, responseText);
-    }
+  /**
+   * Handle the event when the user successfully uploads a file via Dropzone
+   * @prop file [string]
+   * @prop responseText [string]
+   */
+  onDropzoneSuccess: function (file, responseText) {
+    console.log(file, responseText);
+  },
+
+  /**
+   * Handle the avataria generate click
+   */
+  onAvatariaClick: function () {
+    var self = this;
+    var data = {};
+
+    // set gender
+    var gender = self.$avatariaGender.val();
+    if (gender.length > 0) { data.gender = gender; }
+
+    // set species
+    var species = self.$avatariaSpecies.val();
+    if (species.length > 0) { data.species = species; }
+
+    $.post(ROUTES.AVATARIA, data).then(function (res) {
+      self.$avatariaOutput.attr('src', 'data:image/jpg;base64,' + res.base64);
+    });
+  }
 };
 
 $(function() {
-    var page = new CompendiaPage($(".js-editPage"));
+  var page = new CompendiaPage($('.js-editPage'));
 })
